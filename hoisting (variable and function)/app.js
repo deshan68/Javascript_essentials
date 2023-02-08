bf7da740eabfcadfcac0aefd69fb8ad8cbf01b41
“deshan68” <arundeshan@gmail.com>
2023-02-08 08:04:25 -0800

    hoisting

---
+++ hoisting (variable and function)/app.js
@@ -0,0 +1,17 @@
+getName();
+console.log(x);
+
+var x = 7;
+function getName() {
+  console.log("Arun");
+}
+
+//---------------output
+// Arun
+// undefined
+
+// Hoisting is JavaScript's default behavior of moving declarations to the top.
+
+// In JavaScript, a variable/fucntion can be declared after it has been used.
+
+// In other words; a variable/function can be used before it has been declared.
